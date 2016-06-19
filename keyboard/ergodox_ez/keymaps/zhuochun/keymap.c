efc14942cd549887fda97639143895db2f944aea
Zhuochun <zhuochun@hotmail.com>
2016-06-19 14:06:45 +0800

    Separate CALC from SYMB

--- keyboard/ergodox_ez/keymaps/zhuochun/keymap.c
+++ keyboard/ergodox_ez/keymaps/zhuochun/keymap.c
@@ -5,12 +5,13 @@
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
+#define CALC 3 // number pads keys
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Grv    |   1  |   2  |   3  |   4  |   5  | Del  |           |  =   |   6  |   7  |   8  |   9  |   0  |   -    |
+ * | Grv    |   1  |   2  |   3  |   4  |   5  | Del  |           | L3   |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | L1   |           | L2   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -41,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                     KC_PGUP,
                                                      KC_BSPC,KC_SPC,KC_PGDN,
         // right hand
-        KC_EQL,       KC_6, KC_7,  KC_8,   KC_9,   KC_0,    KC_MINS,
+        TG(CALC),     KC_6, KC_7,  KC_8,   KC_9,   KC_0,    KC_MINS,
         TG(MDIA),     KC_Y, KC_U,  KC_I,   KC_O,   KC_P,    KC_BSLS,
                       KC_H, KC_J,  KC_K,   KC_L,   KC_SCLN, CTL_T(KC_QUOT),
         MEH_T(KC_NO), KC_N, KC_M,  KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
@@ -53,16 +54,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Keymap 1: Symbol Layer
  *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   |  | XXXX |           | XXXX |   Up |   7  |   8  |   9  |   +  |   F12  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   -  |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   *  |        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | XXXX |      |      |      |      |                                       |   =  |   0  |   .  |   /  | XXXX |
+ * ,--------------------------------------------------.           ,-------------------------------------------------.
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           | XXXX |  F6  |  F7  |  F8  |  F9  |  F10 |  F11  |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+-------|
+ * |        |   !  |   @  |   {  |   }  |   |  | XXXX |           | XXXX |      |   +  |   {  |   }  |   _  |  F12  |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+-------|
+ * |        |   #  |   $  |   [  |   ]  |   `  |------|           |------|      |   =  |   [  |   ]  |   -  | Enter |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+-------|
+ * |        |   %  |   ^  |   (  |   )  |   ~  |      |           |      |      |      |   (  |   )  |      |       |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+-------'
+ *   | XXXX |      |      |      |      |                                       |      |      |      |      | XXXX |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      | Calc |
@@ -77,27 +78,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
         KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-        KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-        KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
+        KC_TRNS,KC_HASH,KC_DLR, KC_LBRC,KC_RBRC,KC_GRV,
+        KC_TRNS,KC_PERC,KC_CIRC,KC_LPRN,KC_RPRN,KC_TILD,KC_TRNS,
         KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                         KC_TRNS,KC_TRNS,
                                                 KC_TRNS,
                                 KC_TRNS,KC_TRNS,KC_TRNS,
         // right hand
-        KC_TRNS, KC_F6,   KC_F7,  KC_F8, KC_F9,  KC_F10,  KC_F11,
-        KC_TRNS, KC_UP,   KC_7,   KC_8,  KC_9,   KC_PLUS, KC_F12,
-                 KC_DOWN, KC_4,   KC_5,  KC_6,   KC_MINS, KC_TRNS,
-        KC_TRNS, KC_AMPR, KC_1,   KC_2,  KC_3,   KC_ASTR, KC_TRNS,
-                          KC_EQL, KC_0,  KC_DOT, KC_SLSH, KC_TRNS,
-        KC_TRNS, KC_CALC,
+        KC_TRNS,KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,
+        KC_TRNS,KC_TRNS,KC_PLUS,KC_LCBR,KC_RCBR,KC_UNDS,KC_F12,
+                KC_TRNS,KC_EQL, KC_LBRC,KC_RBRC,KC_MINS,KC_ENT,
+        KC_TRNS,KC_TRNS,KC_TRNS,KC_LPRN,KC_RPRN,KC_TRNS,KC_TRNS,
+                        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+        KC_TRNS,KC_TRNS,
         KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS,KC_TRNS, KC_TRNS
 ),
 
 /* Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |      |           | XXXX |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      | MsUp |      |      |      | XXXX |           | XXXX |      | Lclk | Rclk |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -136,6 +137,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,
         KC_TRNS, KC_WBAK, KC_WFWD
 ),
+
+/* Keymap 3: Calculator Layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |           | XXXX |      |      |      |      |      |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |      |      |      |      |      | XXXX |           | XXXX |   Up |   7  |   8  |   9  |   +  |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |------|           |------| Down |   4  |   5  |   6  |   -  |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |   &  |   1  |   2  |   3  |   *  |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   | XXXX |      |      |      |      |                                       |   =  |   0  |   .  |   /  | XXXX |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      | Calc |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+// Number pad keys
+[CALC] = KEYMAP(
+        // left hand
+        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+                                        KC_TRNS,KC_TRNS,
+                                                KC_TRNS,
+                                KC_TRNS,KC_TRNS,KC_TRNS,
+        // right hand
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_PLUS, KC_TRNS,
+                 KC_DOWN, KC_4,    KC_5,    KC_6,    KC_MINS, KC_TRNS,
+        KC_TRNS, KC_AMPR, KC_1,    KC_2,    KC_3,    KC_ASTR, KC_TRNS,
+                          KC_EQL,  KC_0,    KC_DOT,  KC_SLSH, KC_TRNS,
+        KC_TRNS, KC_CALC,
+        KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS
+),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
@@ -173,10 +217,13 @@ void matrix_scan_user(void) {
     ergodox_right_led_3_off();
 
     switch (layer) {
-        case SYMB: // green light
+        case SYMB: // red light
+            ergodox_right_led_1_on();
+            break;
+        case MDIA: // green light
             ergodox_right_led_2_on();
             break;
-        case MDIA: // blue light
+        case CALC: // blue light
             ergodox_right_led_3_on();
             break;
         default: // none
