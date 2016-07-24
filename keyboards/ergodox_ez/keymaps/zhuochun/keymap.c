6758ea708c7c567855e7138a5981209df1e4809a
Zhuochun <zhuochun@hotmail.com>
2016-07-24 12:32:53 +0800

    Adjust square brackets

--- keyboards/ergodox_ez/keymaps/zhuochun/keymap.c
+++ keyboards/ergodox_ez/keymaps/zhuochun/keymap.c
@@ -24,17 +24,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L3  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Esc/Gui|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |Ent/Ctrl|
- * |--------+------+------+------+------+------|  [{  |           |  }]  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *    |Hyper| LCtl | LAlt | Left | Rght |                                       |   =  |  Up  | Down |   '  | Meh |
+ *    |Hyper| LCtl | LAlt | Left | Rght |                                       |   =  |   [  |   ]  |   '  | Meh |
  *    `---------------------------------'                                       `---------------------------------'
  *                                       ,--------------.       ,---------------.
- *                                       | Space | Enter|       |  Up  |  Down  |
+ *                                       | Space | Enter|       |      |        |
  *                               ,-------|-------|------|       |------+--------+-------.
- *                               |       |       |  Up  |       |      |        |       |
+ *                               |       |       |  Up  |       | Down |        |       |
  *                               |Bkspace| LGui  |------|       |------|  -/Gui | Space |
- *                               |       |       | Down |       |      |        |       |
+ *                               |       |       | Down |       |  Up  |        |       |
  *                               `----------------------'       `-----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,       KC_1,    KC_2,    KC_3,    KC_4, KC_5,   KC_NO,
         KC_TAB,       KC_Q,    KC_W,    KC_E,    KC_R, KC_T,   TG(SYMB),
         GUI_T(KC_ESC),KC_A,    KC_S,    KC_D,    KC_F, KC_G,
-        KC_LSFT,      KC_Z,    KC_X,    KC_C,    KC_V, KC_B,   KC_LBRC,
+        KC_LSFT,      KC_Z,    KC_X,    KC_C,    KC_V, KC_B,   KC_NO,
         ALL_T(KC_NO), KC_LCTL, KC_LALT, KC_LEFT,KC_RGHT,
                                                        KC_SPC, KC_ENT,
                                                                KC_UP,
@@ -53,11 +53,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TG(MDIA), KC_6, KC_7,  KC_8,   KC_9,   KC_0,    KC_DELT,
         TG(CALC), KC_Y, KC_U,  KC_I,   KC_O,   KC_P,    KC_BSLS,
                   KC_H, KC_J,  KC_K,   KC_L,   KC_SCLN, CTL_T(KC_ENT),
-        KC_RBRC,  KC_N, KC_M,  KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
-                        KC_EQL,KC_UP,  KC_DOWN,KC_QUOT, MEH_T(KC_NO),
-        KC_UP,KC_DOWN,
-        KC_NO,
-        KC_NO,GUI_T(KC_MINS),KC_SPC
+        KC_NO,    KC_N, KC_M,  KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
+                        KC_EQL,KC_LBRC,KC_RBRC,KC_QUOT, MEH_T(KC_NO),
+        KC_NO,KC_NO,
+        KC_DOWN,
+        KC_UP,GUI_T(KC_MINS),KC_SPC
 ),
 
 /* Keymap 1: Symbol Layer
